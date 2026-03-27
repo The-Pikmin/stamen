72a9d0612d7f58079ee9e4e899d8154c480327a6
Jerry He <118411012+jerhe3@users.noreply.github.com>
2026-03-27 01:54:10 -0400

    Fix backend black and flake8 issues (#9)

--- users/services.py
+++ users/services.py
@@ -115,6 +115,8 @@ def get_image_url(plant_image: PlantImage) -> str:
 def delete_storage_object(supabase_path: str) -> None:
     client = get_supabase_client()
     client.storage.from_(settings.SUPABASE_BUCKET).remove([supabase_path])
+
+
 def _get_id_token(audience: str) -> str:
     # Fetches a Google OIDC identity token
     auth_req = google.auth.transport.requests.Request()

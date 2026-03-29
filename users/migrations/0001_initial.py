5464a1080e092ebcfcf20ea3596e570652dcd3ad
Jerry He <jerryhe365@gmail.com>
2026-03-29 00:34:16 -0400

    remove plant image

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -13,30 +13,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name="PlantImage",
-            fields=[
-                (
-                    "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
-                ("supabase_path", models.CharField(max_length=500)),
-                ("uploaded_at", models.DateTimeField(auto_now_add=True)),
-                (
-                    "user",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        to=settings.AUTH_USER_MODEL,
-                    ),
-                ),
-            ],
-        ),
-        migrations.CreateModel(
+       migrations.CreateModel(
             name="UserProfile",
             fields=[
                 (

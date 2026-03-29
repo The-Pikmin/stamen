b586c327cde0f44a9d19cfc80d62d221538a1b7e
Jerry He <jerryhe365@gmail.com>
2026-03-29 00:34:45 -0400

    black fix

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-       migrations.CreateModel(
+        migrations.CreateModel(
             name="UserProfile",
             fields=[
                 (
